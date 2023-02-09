e34b21e4643761e249949d9ab0fa32003c0a4e4c
GregoireALP <gregoire.alperovitch@gmail.com>
2023-02-09 17:37:10 +0100

    Minor changes

--- main.py
+++ main.py
@@ -5,18 +5,14 @@ import utils.constants as c
 if __name__ == "__main__":
     pygame.init()
 
-    # Import background image
-    backgroundPicture = pygame.image.load("assets/background.png")
-    backgroundPicture = pygame.transform.scale(backgroundPicture, (w.wUtils.window_params["width"], w.wUtils.window_params["height"]))
-
     """
-    MAIN GAME LOOP
+    GAME LOOP
     """
     isRunning = True
     while isRunning:
 
         # Setting background image
-        w.WINDOW.blit(backgroundPicture, (0, 0))
+        w.WINDOW.blit(w.backgroundPicture, (0, 0))
 
         # Check for all current events
         for e in pygame.event.get():
